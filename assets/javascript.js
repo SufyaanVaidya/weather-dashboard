0919336c10e9ae4424d0aba8f04ebf1a6951c0cc
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-03 19:11:19 -0400

    added some fetch

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,5 +1,69 @@
+// i need my variables
 const search = $('.searchbutton');
 const myApiKey = "34c3e6b2f1df4ec6c700399d6355c7df";
 const userInput = $('.userinput');
-const DayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + myApiKey;
-const FiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&appid=" + myApiKey;
+const DayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput.val() + "&Appid=" + myApiKey + "&units=imperial";
+const FiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput.val() + "&ppid=" + myApiKey + "&units=imperial";
+var keyCount = 0;
+
+
+search.on('click', function () {
+
+const typedSearch = userInput.val();
+const DayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + typedSearch + "&Appid=" + myApiKey + "&units=imperial";
+const FiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + typedSearch + "&ppid=" + myApiKey + "&units=imperial";
+if (!typedSearch == "") {
+    console.log(typedSearch);
+    fetch(DayURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        const 
+    });
+}
+
+
+
+
+// i need a way to add it to the searched list
+
+
+
+
+
+
+
+
+// i need a way to display the current forecast
+
+
+
+
+
+
+
+
+// i need a function that shows the 5 day forecast
+
+
+
+
+
+
+
+// i need a function that displays the uv indext on all the items
+
+
+
+
+
+
+
+
+
+
+
+});
+// i need to make it loop throught the data
