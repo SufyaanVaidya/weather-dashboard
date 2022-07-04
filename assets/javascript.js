c57795ab3ba67083737f4d32bf924dce045bf007
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-03 22:43:31 -0400

    added and fixed 5 day forecast

--- assets/javascript.js
+++ assets/javascript.js
@@ -8,7 +8,7 @@ const chosenName = currentWeather.append('<p>');
 const temprature = chosenName.append('<p>');
 const fiveForecastArea = $('.fiveforecastarea').addClass('forecast-body');
 const fiveDayForecast = $('.fivedayforecast').addClass('forecast-text');
-const days = [0,1,2,3,4];
+const days = [1, 2, 3, 4, 5];
 var keyCount = 0;
 
 
@@ -16,7 +16,7 @@ var keyCount = 0;
 search.on('click', function () {
 const typedSearch = userInput.val();
 const DayURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + typedSearch + '&Appid=' + myApiKey + '&units=imperial';
-const FiveDay = 'https://api.openweathermap.org/data/2.5/forecast?q=' + typedSearch + '&ppid=' + myApiKey + '&units=imperial';
+const FiveDay = 'https://api.openweathermap.org/data/2.5/forecast?q=' + typedSearch + '&Appid=' + myApiKey + '&units=imperial';
 if (!typedSearch == '') {
     console.log(typedSearch);
     fetch(DayURL)
@@ -56,27 +56,14 @@ if (!typedSearch == '') {
         return response.json();
     })
     .then(function (data) {
+        console.log(data);
         fiveDayForecast.empty();
-        days.forEach(function(i) {
-            const fiveDayTime = new Date(data.list[i].dt * 1000);
-            fiveDayTime = fiveDayTime.toLocaleDateString('en-US');
-            fiveDayForecast.append('<div> class=forecastcolor'+ '<p>' + fiveDayTime + '</p>' + `<img src='https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png'>` + '<p>' + 'Temp- ' + data.list[i].main.temp + '°F' + '</p>' + '<p>' + 'Humidity- ' + data.list[i].main.humidity + '%' + '</p>' + '</div>');
+        days.forEach(function (i) {
+            const fiveDayTime = moment().add(i + 1, 'days').format('MM/DD/YYYY');
+            fiveDayForecast.append('<div class=forecastcolor>'+ '<p>' + fiveDayTime + '</p>' + `<img src='https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png'>` + '<p>' + 'Temp- ' + data.list[i].main.temp + '°F' + '</p>' + '<p>' + 'Humidity- ' + data.list[i].main.humidity + '%' + '</p>' + '</div>');
         })
-    })
+    });
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 });
-// i need to make it loop throught the data
