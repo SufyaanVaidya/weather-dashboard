7e790c4758e2e81cc954aeac5f7e55bd16c99494
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-07-03 20:04:36 -0400

    displayed weather image and temp

--- assets/javascript.js
+++ assets/javascript.js
@@ -2,13 +2,15 @@
 const search = $('.searchbutton');
 const myApiKey = "34c3e6b2f1df4ec6c700399d6355c7df";
 const userInput = $('.userinput');
-const DayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput.val() + "&Appid=" + myApiKey + "&units=imperial";
-const FiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput.val() + "&ppid=" + myApiKey + "&units=imperial";
-var keyCount = 0;
 const theCityName = $('.city-group').addClass('cityListGroup');
+const currentWeather = $('.todayforecast').append('<div>').addClass('card-body');
+const chosenName = currentWeather.append('<p>');
+const temprature = chosenName.append('<p>');
+var keyCount = 0;
+
+
 
 search.on('click', function () {
-
 const typedSearch = userInput.val();
 const DayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + typedSearch + "&Appid=" + myApiKey + "&units=imperial";
 const FiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + typedSearch + "&ppid=" + myApiKey + "&units=imperial";
@@ -22,7 +24,16 @@ if (!typedSearch == "") {
         console.log(data);
         theCityName.append('<li>' + data.name + "<li>");
         const storage = localStorage.setItem(keyCount, data.name);
-        keyCount = keyCount + 1
+        keyCount = keyCount + 1;
+        currentWeather.empty();
+        currentWeather.append(chosenName);
+
+        const time = new Date(data.dt * 1000);
+        chosenName.append(data.name + ' ' + time.toLocaleDateString('en-US'));
+        chosenName.append(`<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`);
+        chosenName.append(temprature);
+        temprature.append('<p>' + 'Temp- ' + data.main.temp + '<p>');
+
     });
 }
 
